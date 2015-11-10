5d38ee244e238341c17d08f0ed1d66e8e86f363f
Dmytro Ulanovych <outsidereal@gmail.com>
2015-11-10 13:14:10 +0100

    removed Optional from return types

--- src/main/java/com/epam/training/auction/common/UsersService.java
+++ src/main/java/com/epam/training/auction/common/UsersService.java
@@ -1,7 +1,6 @@
 package com.epam.training.auction.common;
 
 import java.io.Serializable;
-import java.util.Optional;
 
 /**
  * Created by Ahmed Magdy <ahmed_magdy@epam.com> on 29.10.15.
@@ -12,7 +11,7 @@ public interface UsersService extends Serializable {
      */
     Long addUser(UserTransferObject user);
 
-    Optional<UserTransferObject> getUserByName(String userName);
+    UserTransferObject getUserByName(String userName);
 
-    Optional<UserTransferObject> getUserById(long id);
+    UserTransferObject getUserById(long id);
 }
